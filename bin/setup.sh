51604c71bba24c39a40489c04d0de7b288279147
Ramon de la Fuente <ramon@future500.nl>
2018-07-13 23:17:14 +0200

    Update the docs and add a default parameter.yml.dist file

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+cp -n docker/parameters.yml.dist app/config/parameters.yml
 docker-compose run php-fpm composer install
 docker-compose run php-fpm bin/console doctrine:migrations:migrate --no-interaction
 docker run --rm -ti -v $(pwd):/src:rw mkenney/npm:node-6.9-debian /usr/local/bin/npm install
