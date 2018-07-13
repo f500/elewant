6fe41338bc2c3e47320a1a01b973069134ea2627
Ramon de la Fuente <ramon@future500.nl>
2018-07-13 21:57:42 +0200

    Add a very basic docker setup

---
+++ bin/setup.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+docker-compose run php-fpm composer install
+docker-compose run php-fpm bin/console doctrine:migrations:migrate --no-interaction
+docker run --rm -ti -v $(pwd):/src:rw mkenney/npm:node-6.9-debian /usr/local/bin/npm install
+docker run --rm -ti -v $(pwd):/src:rw mkenney/npm:node-6.9-debian /usr/local/bin/grunt
