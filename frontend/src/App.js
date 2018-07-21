8cf8b2db8cfa6e5295581bc5889e33682f78f295
Jasper N. Brouwer <jasper@nerdsweide.nl>
2018-07-21 10:47:09 +0200

    Build Tic Tac Toe (tutorial)
    See https://reactjs.org/tutorial/tutorial.html

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,9 @@
 import React, { Component } from 'react';
-import { Card, CardBody, CardTitle, CardText } from 'reactstrap';
-import logo from './logo.svg';
-import './App.scss';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCoffee, faThumbsUp, faFlag } from '@fortawesome/free-solid-svg-icons';
+import Game from './TicTacToe';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-
-        <div className="container">
-          <div className="row justify-content-center">
-            <div className="col-xs">
-              <Card>
-                <CardBody>
-                  <CardTitle className="App-title">
-                    <FontAwesomeIcon icon={faCoffee} />
-                    <FontAwesomeIcon icon={faFlag} />
-                    <FontAwesomeIcon icon={faThumbsUp} />
-                  </CardTitle>
-                  <CardText>
-                    To get started, edit <code>src/App.js</code> and save to reload.
-                  </CardText>
-                </CardBody>
-              </Card>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
+    return <Game />;
   }
 }
 
---
+++ frontend/src/TicTacToe/index.js
@@ -0,0 +1,135 @@
+import { Button, Container } from 'reactstrap';
+import React from 'react';
+
+import './index.scss';
+
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+
+function Board(props) {
+  const renderSquare = i => {
+    return <Square value={props.squares[i]} onClick={() => props.onClick(i)} />;
+  };
+
+  return (
+    <div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
+}
+
+class Game extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      history: [
+        {
+          squares: Array(9).fill(null)
+        }
+      ],
+      stepNumber: 0,
+      xIsNext: true
+    };
+  }
+
+  handleClick(i) {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+
+    let squares = current.squares.slice();
+    let xIsNext = this.state.xIsNext;
+
+    if (squares[i] || this.calculateWinner(squares)) {
+      return;
+    }
+
+    squares[i] = xIsNext ? 'X' : 'O';
+    xIsNext = !xIsNext;
+
+    this.setState({
+      history: history.concat([{ squares }]),
+      xIsNext,
+      stepNumber: history.length
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: step % 2 === 0
+    });
+  }
+
+  calculateWinner(squares) {
+    const lines = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+
+    return null;
+  }
+
+  render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = this.calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move ? 'Go to move #' + move : 'Go to game start';
+
+      return (
+        <li key={move}>
+          <Button outline size="sm" onClick={() => this.jumpTo(move)}>{desc}</Button>
+        </li>
+      );
+    });
+
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
+    return (
+      <Container>
+        <div className="game">
+          <div className="game-board">
+            <Board squares={current.squares} onClick={i => this.handleClick(i)} />
+          </div>
+          <div className="game-info">
+            <div>{status}</div>
+            <ol>{moves}</ol>
+          </div>
+        </div>
+      </Container>
+    );
+  }
+}
+
+export default Game;
