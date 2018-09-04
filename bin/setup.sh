1615c355099c63f77b77e099374959fe0ab57a25
Ramon de la Fuente <ramon@future500.nl>
2018-09-04 14:05:39 +0200

    Update the docker setup end default environments

--- bin/setup.sh
+++ bin/setup.sh
@@ -2,6 +2,6 @@
 
 docker-compose run php-fpm composer install
 docker-compose run php-fpm bin/console doctrine:migrations:migrate --no-interaction
-docker-compose run php-fpm bin/console event-store:event-stream:creat --no-interaction
+docker-compose run php-fpm bin/console event-store:event-stream:create --no-interaction
 docker run --rm -ti -v $(pwd):/src:rw mkenney/npm:node-6.9-debian /usr/local/bin/npm install
 docker run --rm -ti -v $(pwd):/src:rw mkenney/npm:node-6.9-debian /usr/local/bin/grunt
