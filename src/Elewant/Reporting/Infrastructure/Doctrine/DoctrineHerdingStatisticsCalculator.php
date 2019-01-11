eff2771159f2dbe4f5706a9a2bc32c5ed3b0c471
edwinkortman <edwin@future500.nl>
2019-01-11 12:54:08 +0100

    Modify statistics calculator to use NumberOf repository

--- src/Elewant/Reporting/Infrastructure/Doctrine/DoctrineHerdingStatisticsCalculator.php
+++ src/Elewant/Reporting/Infrastructure/Doctrine/DoctrineHerdingStatisticsCalculator.php
@@ -5,60 +5,29 @@ declare(strict_types=1);
 namespace Elewant\Reporting\Infrastructure\Doctrine;
 
 use DateTimeInterface;
-use Doctrine\Common\Collections\Collection;
-use Doctrine\Common\Collections\Criteria;
 use Elewant\Reporting\DomainModel\CalculatedHerdingStatistics;
 use Elewant\Reporting\DomainModel\HerdingStatisticsCalculator;
-use Elewant\Webapp\DomainModel\Herding\Herd;
-use Elewant\Webapp\DomainModel\Herding\HerdRepository;
+use Elewant\Reporting\DomainModel\Statistics\NumberOf;
 
-/** @todo Is it ok to use the "Webapp" bounded-context here? */
 final class DoctrineHerdingStatisticsCalculator implements HerdingStatisticsCalculator
 {
     /**
-     * @var HerdRepository
+     * @var NumberOf
      */
-    private $herdRepository;
+    private $numberOf;
 
-    public function __construct(HerdRepository $herdRepository)
+    public function __construct(NumberOf $numberOf)
     {
-        $this->herdRepository = $herdRepository;
+        $this->numberOf = $numberOf;
     }
 
     public function generate(DateTimeInterface $from, DateTimeInterface $to): CalculatedHerdingStatistics
     {
-        $herds = $this->herdsCreatedBetween($from, $to);
-
         return new CalculatedHerdingStatistics(
             $from,
             $to,
-            $this->numberOfNewHerds($herds),
-            $this->numberOfNewElePHPants($herds)
+            $this->numberOf->newHerdsFormedBetween($from, $to),
+            $this->numberOf->newElePHPantsAdoptedBetween($from, $to)
         );
-    }
-
-    private function numberOfNewHerds(Collection $herds): int
-    {
-        return count($herds);
-    }
-
-    private function numberOfNewElePHPants(Collection $herds): int
-    {
-        return array_reduce(
-            $herds->toArray(),
-            function (int $totalCount, Herd $herd): int {
-                return $totalCount + count($herd->elePHPants());
-            },
-            0
-        );
-    }
-
-    private function herdsCreatedBetween(DateTimeInterface $from, DateTimeInterface $to): Collection
-    {
-        $criteria = new Criteria();
-        $criteria->where($criteria->expr()->gt('formedOn', $from))
-            ->andWhere($criteria->expr()->lte('formedOn', $to));
-
-        return $this->herdRepository->matching($criteria);
     }
 }
