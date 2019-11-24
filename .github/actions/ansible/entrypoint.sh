b752f084d6f4ec4053e889d7b1221a41d091cfc3
Ramon de la Fuente <ramon@future500.nl>
2019-11-24 22:07:44 +0100

    Add a github workflow for deployment

---
+++ .github/actions/ansible/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+
+# Write the password file
+echo "$VAULT_PASS" > ~/.vault_pass.txt
+mkdir ~/.ssh
+ansible-vault --vault-password-file ~/.vault_pass.txt view ansible/deploy/deploy.secret > ~/.ssh/id_rsa
+chmod 0600 ~/.ssh/id_rsa
+
+ansible-playbook ansible/deploy/playbook.yml \
+    -i ansible/hosts-deploy \
+    --vault-password-file ~/.vault_pass.txt \
+    --key-file ~/.ssh/id_rsa \
+    ${INPUT_EXTRAVARS} \
+    ${INPUT_VERBOSITY}
