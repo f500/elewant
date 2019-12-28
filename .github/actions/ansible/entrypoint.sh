56b024bd425e408a9e36c0048fdb42a525955f4e
Ramon de la Fuente <ramon@future500.nl>
2019-12-28 01:13:12 +0100

    Fix deployment github action

--- .github/actions/ansible/entrypoint.sh
+++ .github/actions/ansible/entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 # Write the password file
 echo "$VAULT_PASS" > ~/.vault_pass.txt
 mkdir ~/.ssh
-ansible-vault --vault-password-file ~/.vault_pass.txt view ansible/deploy/deploy.secret > ~/.ssh/id_rsa
+ansible-vault view --vault-password-file ~/.vault_pass.txt ansible/deploy/deploy.secret > ~/.ssh/id_rsa
 chmod 0600 ~/.ssh/id_rsa
 
 ansible-playbook ansible/deploy/playbook.yml \
